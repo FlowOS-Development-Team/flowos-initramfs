e5d3837a0a2bbcad1f45cffe3bb4c4aae107deb4
root <root@citadel.local.favoriteone.dev>
2026-01-16 21:02:18 -0800

    added fstab with qemu testing dev id, added in dev environment a cp line to copy the compiles initram to the linux kernel and modified the init file

--- initramfs_compress.sh
+++ initramfs_compress.sh
@@ -1,2 +1,3 @@
-find . -print0 | cpio --null -ov --format=newc > /initramfs.cpio
+find . -print0 | cpio --null -ov --format=newc > ./initramfs.cpio
 gzip -9 initramfs.cpio
+cp initramfs.cpio.gz /srv/source/linux-6.9/initramfs.cpio.gz
