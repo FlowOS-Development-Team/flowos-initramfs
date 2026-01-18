7007c96ad831f1b9a5f6b01d32687b248fd4c97a
root <root@citadel.local.favoriteone.dev>
2026-01-17 18:02:36 -0800

    fixed a mnt to use virtio instead

--- initramfs_compress.sh
+++ initramfs_compress.sh
@@ -1,3 +1,4 @@
+sudo rm -rf initramfs.cpio.gz
 find . -print0 | cpio --null -ov --format=newc > ./initramfs.cpio
 gzip -9 initramfs.cpio
 cp initramfs.cpio.gz /srv/source/linux-6.9/initramfs.cpio.gz
